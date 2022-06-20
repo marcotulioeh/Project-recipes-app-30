fcce8cf9e1fdf6da998e891501449a9179f91d9b
saraivais <isadorasaraiva95@gmail.com>
2022-06-20 17:14:05 -0300

    feat: adiciona state do componente p/ controlar inputs~ handleChange genérico, baseado em id e value, atualiza estado com valores do input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,33 +4,44 @@ class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      // loginEmail: '',
-      // loginPassword: '',
-      // isButtonDisabled: true,
+      loginEmail: '',
+      loginPassword: '',
+      isButtonDisabled: true,
     };
   }
 
+  handleInputsChange = ({ target }) => {
+    const { id, value } = target;
+    this.setState({ [id]: value });
+  }
+
   render() {
+    const { loginEmail, loginPassword, isButtonDisabled } = this.state;
     return (
       <>
-        <label htmlFor="email-input">
+        <label htmlFor="loginEmail">
           <input
             type="text"
-            id="email-input"
+            id="loginEmail"
             data-testid="email-input"
+            value={ loginEmail }
+            onChange={ this.handleInputsChange }
 
           />
         </label>
-        <label htmlFor="password-input">
+        <label htmlFor="loginPassword">
           <input
             type="password"
-            id="password-input"
+            id="loginPassword"
             data-testid="password-input"
+            value={ loginPassword }
+            onChange={ this.handleInputsChange }
           />
         </label>
         <button
           type="button"
           data-testid="login-submit-btn"
+          disabled={ isButtonDisabled }
         >
           Enter
         </button>
