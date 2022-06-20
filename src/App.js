2a8fd904e91e669a18544849649a9a0126743dfb
saraivais <isadorasaraiva95@gmail.com>
2022-06-20 17:01:40 -0300

    feat: adiciona todas as rotas~ ainda sem componente, só para registrar a existência

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      {/* Rotas abaixo ainda serão implementadas, só um esqueleto */}
+      <Route path="/foods" />
+      <Route path="/drinks" />
+      <Route path="/foods/:id-da-receita" />
+      <Route path="/drinks/:id-da-receita" />
+      <Route path="/foods/:id-da-receita/in-progress" />
+      <Route path="/drinks/:id-da-receita/in-progress" />
+      <Route path="/explore" />
+      <Route path="/explore/foods" />
+      <Route path="/explore/drinks" />
+      <Route path="/explore/foods/ingredients" />
+      <Route path="/explore/drinks/ingredients" />
+      <Route path="/explore/foods/nationalities" />
+      <Route path="/profile" />
+      <Route path="/done-recipes" />
+      <Route path="/favorite-recipes" />
     </Switch>
   );
 }
