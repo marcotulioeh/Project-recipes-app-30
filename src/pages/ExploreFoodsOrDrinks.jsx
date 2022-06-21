78c4d8ff0f72a3b652cc2821968d1579a5f4caa7
Jazyel99 <jaziel@outlook.com>
2022-06-21 19:03:49 -0300

    feat: adicionado condição caso a rota seja explore/foods irá aparecer o botão By Natinality se não, o botão não será renderizado

--- src/pages/ExploreFoodsOrDrinks.jsx
+++ src/pages/ExploreFoodsOrDrinks.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const ExploreFoodsOrDrinks = () => (
-  <>
-    <button
-      type="button"
-      data-testid="explore-by-ingredient"
-    >
-      By Ingredient
-    </button>
-    <button
-      type="button"
-      data-testid="explore-by-nationality"
-    >
-      By Nationality
-    </button>
-    <button
-      type="button"
-      data-testid="explore-surprise"
-    >
-      Surprise me!
-    </button>
-  </>
-);
+const ExploreFoodsOrDrinks = ({ history }) => {
+  const { pathname } = history.location;
+  return (
+    <>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        By Ingredient
+      </button>
+      {
+        pathname !== '/explore/drinks' && (
+          <button
+            type="button"
+            data-testid="explore-by-nationality"
+          >
+            By Nationality
+          </button>
+        )
+      }
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
+    </>
+  );
+};
+
+ExploreFoodsOrDrinks.propTypes = {
+  history: propTypes.shape().isRequired,
+  pathname: propTypes.string.isRequired,
+};
 
 export default ExploreFoodsOrDrinks;
