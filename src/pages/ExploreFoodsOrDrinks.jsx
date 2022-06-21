28af2c7d810ce9e5891381b025dc2b2a4a7a8c84
Jazyel99 <jaziel@outlook.com>
2022-06-21 18:51:41 -0300

    feat: adicionado os botões para tela de explorar foods e drinks

--- src/pages/ExploreFoodsOrDrinks.jsx
+++ src/pages/ExploreFoodsOrDrinks.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 
 const ExploreFoodsOrDrinks = () => (
   <>
+    <button
+      type="button"
+      data-testid="explore-by-ingredient"
+    >
+      By Ingredient
+    </button>
+    <button
+      type="button"
+      data-testid="explore-by-nationality"
+    >
+      By Nationality
+    </button>
+    <button
+      type="button"
+      data-testid="explore-surprise"
+    >
+      Surprise me!
+    </button>
   </>
 );
 
