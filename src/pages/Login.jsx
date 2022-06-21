f787459a1fb1c98c3d799dbd450dbdd523313379
saraivais <isadorasaraiva95@gmail.com>
2022-06-21 15:58:21 -0300

    fix (Login): transforma componente de classe em componente funcional~

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,85 +1,131 @@
-import React from 'react';
-import propTypes from 'prop-types';
-import {
-  saveTokensToLocalStorage,
-  saveUserEmailToLocalStorage,
-} from '../helpers/loginLocalStorage';
+import React, { useEffect, useState } from 'react';
 
-class Login extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loginEmail: '',
-      loginPassword: '',
-      isButtonDisabled: true,
-    };
-  }
+// import { useHistory } from 'react-router-dom';
+// import {
+//   saveTokensToLocalStorage,
+//   saveUserEmailToLocalStorage,
+// } from '../helpers/loginLocalStorage';
 
-  handleInputsChange = ({ target }) => {
-    const { id, value } = target;
-    this.setState(
-      () => ({ [id]: value }),
-      this.validadeInputsToEnableButton,
-    );
-  }
+// class Login extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       loginEmail: '',
+//       loginPassword: '',
+//       isButtonDisabled: true,
+//     };
+//   }
 
-  submitButton = () => {
-    const { loginEmail } = this.state;
-    const { history } = this.props;
-    saveTokensToLocalStorage();
-    saveUserEmailToLocalStorage(loginEmail);
-    history.push('/foods');
-  }
+//   handleInputsChange = ({ target }) => {
+//     const { id, value } = target;
+//     this.setState(
+//       () => ({ [id]: value }),
+//       this.validadeInputsToEnableButton,
+//     );
+//   }
 
-  validadeInputsToEnableButton = () => {
-    const { loginEmail, loginPassword } = this.state;
+//   submitButton = () => {
+//     const { loginEmail } = this.state;
+//     const { history } = this.props;
+//     saveTokensToLocalStorage();
+//     saveUserEmailToLocalStorage(loginEmail);
+//     history.push('/foods');
+//   }
+
+//   validadeInputsToEnableButton = () => {
+//     const { loginEmail, loginPassword } = this.state;
+//     const minimumLengthPassword = 6;
+//     const emailValidation = /\S+@\S+\.\S+/;
+//     const shouldButtonBeEnabled = emailValidation.test(loginEmail)
+//       && loginPassword.length > minimumLengthPassword;
+//     this.setState({ isButtonDisabled: !shouldButtonBeEnabled });
+//   }
+
+//   render() {
+//     const { loginEmail, loginPassword, isButtonDisabled } = this.state;
+//     return (
+//       <>
+//         <label htmlFor="loginEmail">
+//           <input
+//             type="text"
+//             id="loginEmail"
+//             data-testid="email-input"
+//             value={ loginEmail }
+//             onChange={ this.handleInputsChange }
+
+//           />
+//         </label>
+//         <label htmlFor="loginPassword">
+//           <input
+//             type="password"
+//             id="loginPassword"
+//             data-testid="password-input"
+//             value={ loginPassword }
+//             onChange={ this.handleInputsChange }
+//           />
+//         </label>
+//         <button
+//           type="button"
+//           data-testid="login-submit-btn"
+//           disabled={ isButtonDisabled }
+//           onClick={ this.submitButton }
+//         >
+//           Enter
+//         </button>
+//       </>
+//     );
+//   }
+// }
+
+// Login.propTypes = {
+//   history: propTypes.shape().isRequired,
+// };
+
+function Login() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [buttonCondition, setButtonCondition] = useState(true);
+
+  useEffect(() => {
     const minimumLengthPassword = 6;
     const emailValidation = /\S+@\S+\.\S+/;
-    // console.log(emailValidation.test(loginEmail));
-    // console.log(loginPassword.length > minimumLengthPassword);
-    const shouldButtonBeEnabled = emailValidation.test(loginEmail)
-      && loginPassword.length > minimumLengthPassword;
-    this.setState({ isButtonDisabled: !shouldButtonBeEnabled });
-  }
+    const shouldButtonBeEnabled = emailValidation.test(userEmail)
+      && userPassword.length > minimumLengthPassword;
 
-  render() {
-    const { loginEmail, loginPassword, isButtonDisabled } = this.state;
-    return (
-      <>
-        <label htmlFor="loginEmail">
-          <input
-            type="text"
-            id="loginEmail"
-            data-testid="email-input"
-            value={ loginEmail }
-            onChange={ this.handleInputsChange }
+    setButtonCondition(shouldButtonBeEnabled);
+  }, [userEmail, userPassword, setButtonCondition]);
 
-          />
-        </label>
-        <label htmlFor="loginPassword">
-          <input
-            type="password"
-            id="loginPassword"
-            data-testid="password-input"
-            value={ loginPassword }
-            onChange={ this.handleInputsChange }
-          />
-        </label>
-        <button
-          type="button"
-          data-testid="login-submit-btn"
-          disabled={ isButtonDisabled }
-          onClick={ this.submitButton }
-        >
-          Enter
-        </button>
-      </>
-    );
-  }
+  return (
+    <>
+      <label htmlFor="loginEmail">
+        <input
+          type="text"
+          id="loginEmail"
+          data-testid="email-input"
+          value={ userEmail }
+          onChange={ ({ target }) => setUserEmail(target.value) }
+
+        />
+      </label>
+      <label htmlFor="loginPassword">
+        <input
+          type="password"
+          id="loginPassword"
+          data-testid="password-input"
+          value={ userPassword }
+          onChange={ ({ target }) => setUserPassword(target.value) }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ buttonCondition }
+        onClick={ () => console.log('a') }
+      >
+        Enter
+      </button>
+    </>
+  );
 }
-
-Login.propTypes = {
-  history: propTypes.shape().isRequired,
-};
 
 export default Login;
