7df53eef074907842af67a53c043afc551224eb0
saraivais <isadorasaraiva95@gmail.com>
2022-06-21 16:02:01 -0300

    feat (Login): utilização do hook useHistory p/ navegação~

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,90 +1,15 @@
 import React, { useEffect, useState } from 'react';
-
-// import { useHistory } from 'react-router-dom';
-// import {
-//   saveTokensToLocalStorage,
-//   saveUserEmailToLocalStorage,
-// } from '../helpers/loginLocalStorage';
-
-// class Login extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       loginEmail: '',
-//       loginPassword: '',
-//       isButtonDisabled: true,
-//     };
-//   }
-
-//   handleInputsChange = ({ target }) => {
-//     const { id, value } = target;
-//     this.setState(
-//       () => ({ [id]: value }),
-//       this.validadeInputsToEnableButton,
-//     );
-//   }
-
-//   submitButton = () => {
-//     const { loginEmail } = this.state;
-//     const { history } = this.props;
-//     saveTokensToLocalStorage();
-//     saveUserEmailToLocalStorage(loginEmail);
-//     history.push('/foods');
-//   }
-
-//   validadeInputsToEnableButton = () => {
-//     const { loginEmail, loginPassword } = this.state;
-//     const minimumLengthPassword = 6;
-//     const emailValidation = /\S+@\S+\.\S+/;
-//     const shouldButtonBeEnabled = emailValidation.test(loginEmail)
-//       && loginPassword.length > minimumLengthPassword;
-//     this.setState({ isButtonDisabled: !shouldButtonBeEnabled });
-//   }
-
-//   render() {
-//     const { loginEmail, loginPassword, isButtonDisabled } = this.state;
-//     return (
-//       <>
-//         <label htmlFor="loginEmail">
-//           <input
-//             type="text"
-//             id="loginEmail"
-//             data-testid="email-input"
-//             value={ loginEmail }
-//             onChange={ this.handleInputsChange }
-
-//           />
-//         </label>
-//         <label htmlFor="loginPassword">
-//           <input
-//             type="password"
-//             id="loginPassword"
-//             data-testid="password-input"
-//             value={ loginPassword }
-//             onChange={ this.handleInputsChange }
-//           />
-//         </label>
-//         <button
-//           type="button"
-//           data-testid="login-submit-btn"
-//           disabled={ isButtonDisabled }
-//           onClick={ this.submitButton }
-//         >
-//           Enter
-//         </button>
-//       </>
-//     );
-//   }
-// }
-
-// Login.propTypes = {
-//   history: propTypes.shape().isRequired,
-// };
+import { useHistory } from 'react-router-dom';
+import {
+  saveTokensToLocalStorage,
+  saveUserEmailToLocalStorage,
+} from '../helpers/loginLocalStorage';
 
 function Login() {
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
   const [buttonCondition, setButtonCondition] = useState(true);
+  const history = useHistory();
 
   useEffect(() => {
     const minimumLengthPassword = 6;
@@ -94,6 +19,13 @@ function Login() {
 
     setButtonCondition(shouldButtonBeEnabled);
   }, [userEmail, userPassword, setButtonCondition]);
+
+  const submitButton = () => {
+    // console.log('CLIQUEI');
+    saveTokensToLocalStorage();
+    saveUserEmailToLocalStorage(userEmail);
+    history.push('/foods');
+  };
 
   return (
     <>
@@ -120,7 +52,7 @@ function Login() {
         type="button"
         data-testid="login-submit-btn"
         disabled={ buttonCondition }
-        onClick={ () => console.log('a') }
+        onClick={ submitButton }
       >
         Enter
       </button>
