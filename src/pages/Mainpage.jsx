6458ecee6a653acfef401c555933a48fc36b05b2
saraivais <isadorasaraiva95@gmail.com>
2022-06-22 13:57:45 -0300

    feat (Mainpage): acesso ao contexto c/ useContext~

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import RecipeAppContext from '../context/RecipeAppContext';
 
 function Mainpage() {
+  const {
+    foods: { mealRecipes }, drinks: { drinkRecipes },
+  } = useContext(RecipeAppContext);
+
+  console.log('mealRecipes', mealRecipes);
+  console.log('drinkRecipes', drinkRecipes);
+  const history = useHistory();
+
+  console.log(history.location.pathname);
   return (<div>mainpage</div>);
+  // requisito 25 em diante~
 }
 
 export default Mainpage;
