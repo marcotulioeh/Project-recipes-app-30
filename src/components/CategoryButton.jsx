3c05c091824504f434cf48ce8a41a845577aba2c
saraivais <isadorasaraiva95@gmail.com>
2022-06-22 17:11:51 -0300

    feat (CategoryButton): implementa toggle de filtros de categoria~

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import propTypes from 'prop-types';
+import RecipeAppContext from '../context/RecipeAppContext';
 
 function CategoryButton(props) {
+  const { filtering: { setCurrentFilter } } = useContext(RecipeAppContext);
   const { props: { categoryName } } = props;
+
+  const toggleThisFilter = (category) => {
+    setCurrentFilter((prevState) => (prevState === category ? '' : category));
+  };
+
   return (
     <button
       type="button"
       data-testid={ `${categoryName}-category-filter` }
+      onClick={ () => toggleThisFilter(categoryName) }
     >
       { categoryName }
     </button>
