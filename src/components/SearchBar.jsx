ef603ce967d2fd38f8a553948c81e7e25324a5e9
saraivais <isadorasaraiva95@gmail.com>
2022-06-22 19:11:14 -0300

    feat (SearchBar): creates text input & radio btns~

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
 function SearchBar() {
-  return (<div>SearchBar</div>);
+  return (
+    <section>
+      <input type="text" name="" id="" />
+
+      <label htmlFor="ingredient-input">
+        Ingredient
+        <input
+          type="radio"
+          name="filter-type"
+          id="ingredient-input"
+          data-testid="ingredient-search-radio"
+        />
+      </label>
+      <label htmlFor="name-input">
+        Name
+        <input
+          type="radio"
+          name="filter-type"
+          id="name-input"
+          data-testid="name-search-radio"
+        />
+      </label>
+      <label htmlFor="first-letter-input">
+        First Letter
+        <input
+          type="radio"
+          name="filter-type"
+          id="first-letter-input"
+          data-testid="first-letter-search-radio"
+        />
+      </label>
+
+      <button type="button" data-testid="exec-search-btn">Search</button>
+
+    </section>);
 }
 
 export default SearchBar;
