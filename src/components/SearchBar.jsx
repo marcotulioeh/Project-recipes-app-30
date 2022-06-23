8b18460918836d34a596d1fb27971ec39b9dc283
saraivais <isadorasaraiva95@gmail.com>
2022-06-22 23:39:00 -0300

    feat (SearchBar): adds state to control search inputs & context to send to provider~

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import RecipeAppContext from '../context/RecipeAppContext';
 
 function SearchBar() {
+  const { searching: { setSearchFilters } } = useContext(RecipeAppContext);
+  const [filterType, setFilterType] = useState('');
+  const [filterString, setFilterString] = useState('');
+
+  const sendSearchFiltersToContext = () => {
+    console.log('cliquei');
+    const newSearchFilters = { type: filterType, string: filterString };
+    setSearchFilters(newSearchFilters);
+  };
+
+  const handleRadioButtonsChange = ({ target }) => {
+    if (target.checked) {
+      setFilterType(target.value);
+    }
+  };
+
   return (
     <section>
-      <input type="text" name="" id="" />
+      <input
+        type="text"
+        data-testid="search-input"
+        value={ filterString }
+        onChange={ ({ target }) => setFilterString(target.value) }
+      />
 
       <label htmlFor="ingredient-input">
         Ingredient
@@ -12,6 +34,8 @@ function SearchBar() {
           name="filter-type"
           id="ingredient-input"
           data-testid="ingredient-search-radio"
+          value="Ingredient"
+          onChange={ (event) => handleRadioButtonsChange(event) }
         />
       </label>
       <label htmlFor="name-input">
@@ -21,19 +45,30 @@ function SearchBar() {
           name="filter-type"
           id="name-input"
           data-testid="name-search-radio"
+          value="Name"
+          onChange={ (event) => handleRadioButtonsChange(event) }
         />
       </label>
       <label htmlFor="first-letter-input">
-        First Letter
+        First letter
         <input
           type="radio"
           name="filter-type"
           id="first-letter-input"
           data-testid="first-letter-search-radio"
+          value="First letter"
+          onChange={ (event) => handleRadioButtonsChange(event) }
         />
       </label>
 
-      <button type="button" data-testid="exec-search-btn">Search</button>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ () => sendSearchFiltersToContext() }
+      >
+        Search
+
+      </button>
 
     </section>);
 }
