e730b6b2a6c8e5633b0342db43208472034c2c66
Jazyel99 <jaziel@outlook.com>
2022-06-29 20:20:01 -0300

    refactor: renomeado o arquivo e inserido os requisitos de 60 ao 65

--- src/components/FavoriteRecipeCard.jsx
+++ src/components/FavoriteRecipeCard.jsx
@@ -1,30 +1,82 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import propTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import copy from 'clipboard-copy';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
+import RecipeAppContext from '../context/RecipeAppContext';
+
+const THREE_MILLISECONDS = 3000;
 
 function FavoriteRecipeCard({ recipe, index, onClick }) {
-  const { image, name, category, nationality } = recipe;
+  const { id, image, name, type, category, nationality, alcoholicOrNot } = recipe;
+
+  const history = useHistory();
+
+  const { messageCopy: { setIsOpenMessageCopy } } = useContext(RecipeAppContext);
+
+  const redirectToDetailsPage = () => {
+    switch (recipe.type) {
+    case ('food'):
+      history.push(`/foods/${recipe.id}`);
+      break;
+    case ('drink'):
+      history.push(`/drinks/${recipe.id}`);
+      break;
+    default:
+      break;
+    }
+  };
+
+  const copyRecipeDetailsLink = () => {
+    setIsOpenMessageCopy(true);
+    setTimeout(() => {
+      setIsOpenMessageCopy(false);
+    }, THREE_MILLISECONDS);
+    copy(`http://localhost:3000/${type}s/${id}`);
+  };
+
   return (
     <div>
-      <img
-        className="recipe-card-image"
-        src={ image }
-        alt={ name }
-        data-testid={ `${index}-horizontal-image` }
-      />
-      <h2
-        data-testid={ `${index}-horizontal-top-text` }
-      >
-        { `${nationality} - ${category}` }
-      </h2>
-      <h4
-        data-testid={ `${index}-horizontal-name` }
-      >
-        { name }
-      </h4>
       <button
         type="button"
+        onClick={ () => redirectToDetailsPage() }
+      >
+        <img
+          className="recipe-card-image"
+          src={ image }
+          alt={ name }
+          data-testid={ `${index}-horizontal-image` }
+        />
+      </button>
+      {
+        alcoholicOrNot ? (
+          <h2
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            { `${alcoholicOrNot}` }
+          </h2>
+        ) : (
+          <h2
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            { `${nationality} - ${category}` }
+          </h2>
+        )
+      }
+      <button
+        type="button"
+        onClick={ () => redirectToDetailsPage() }
+      >
+        <h4
+          data-testid={ `${index}-horizontal-name` }
+        >
+          { name }
+        </h4>
+      </button>
+      <button
+        type="button"
+        onClick={ () => copyRecipeDetailsLink() }
       >
         <img
           data-testid={ `${index}-horizontal-share-btn` }
@@ -48,10 +100,13 @@ function FavoriteRecipeCard({ recipe, index, onClick }) {
 
 FavoriteRecipeCard.propTypes = {
   recipe: propTypes.shape({
+    id: propTypes.string,
     image: propTypes.string,
+    type: propTypes.string,
     name: propTypes.string,
     category: propTypes.string,
     nationality: propTypes.string,
+    alcoholicOrNot: propTypes.string,
   }).isRequired,
   index: propTypes.number.isRequired,
   onClick: propTypes.func.isRequired,
