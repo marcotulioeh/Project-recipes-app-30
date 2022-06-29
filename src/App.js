aa654b9a61024b3fa3b030cdf68ad049a87e4dff
saraivais <isadorasaraiva95@gmail.com>
2022-06-29 14:19:31 -0300

    feat: altera ordem de rotas & adiciona NotFound~

--- src/App.js
+++ src/App.js
@@ -8,34 +8,29 @@ import Explore from './pages/Explore';
 // import ExploreFoodsOrDrinks from './pages/ExploreFoodsOrDrinks';
 import Profile from './pages/Profile';
 import Mainpage from './pages/Mainpage';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
-      {/* Rotas abaixo ainda serão implementadas, só um esqueleto */}
-      <Route path="/foods" component={ Mainpage } />
-      <Route path="/drinks" component={ Mainpage } />
-      <Route path="/foods/:id-da-receita" />
-      <Route path="/drinks/:id-da-receita" />
-      <Route path="/foods/:id-da-receita/in-progress" />
-      <Route path="/drinks/:id-da-receita/in-progress" />
-      <Route
-        path="/explore/foods"
-        component={ Explore }
-      />
-      <Route
-        path="/explore/drinks"
-        component={ Explore }
-      />
+      <Route exact path="/explore/drinks/nationalities" component={ NotFound } />
+      <Route path="/explore/foods/nationalities" component={ Explore } />
       <Route path="/explore/foods/ingredients" component={ Explore } />
       <Route path="/explore/drinks/ingredients" component={ Explore } />
-      <Route path="/explore/foods/nationalities" component={ Explore } />
-      <Route path="/explore" render={ (props) => <Explore { ...props } /> } />
+      <Route path="/explore/foods" component={ Explore } />
+      <Route path="/explore/drinks" component={ Explore } />
+      <Route path="/explore" component={ Explore } />
       <Route path="/profile" />
       <Route path="/profile" component={ Profile } />
       <Route path="/done-recipes" />
       <Route path="/favorite-recipes" />
+      <Route path="/drinks/:id-da-receita/in-progress" />
+      <Route path="/foods/:id-da-receita/in-progress" />
+      <Route path="/drinks/:id-da-receita" />
+      <Route path="/foods/:id-da-receita" />
+      <Route path="/foods" component={ Mainpage } />
+      <Route path="/drinks" component={ Mainpage } />
+      <Route exact path="/" component={ Login } />
     </Switch>
   );
 }
